6e2af34b063d05deec8f464ba755c541ebb87292
unknown <yassine.ennouhi@ynov.com>
2022-09-27 14:30:27 +0200

    Prompt user to chose number of players

--- puissance4/src/Menu.java
+++ puissance4/src/Menu.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class Menu {
-    
+    public static int chooseNbPlayers(){
+        int nbPlayers = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many players will play?");
+        System.out.println("2 or 3?");
+        nbPlayers = sc.nextInt();
+        while(nbPlayers != 2 && nbPlayers != 3){
+            System.out.println("Please enter 2 or 3");
+            nbPlayers = sc.nextInt();
+        }
+        return nbPlayers;
+    }
 }
