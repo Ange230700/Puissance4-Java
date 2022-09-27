7a1588747890471b62570e244469d3e3ce9ebcc9
kangeeri <angeericstephane.kouakou@ynov.com>
2022-09-27 15:40:27 +0200

    réparation du constructeur Grid

--- puissance4/src/App.java
+++ puissance4/src/App.java
@@ -2,8 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         int nbPlayers = Menu.chooseNbPlayers();
         System.out.println("nbPlayers = " + nbPlayers);        
-        Grid grid = new Grid();
-        grid.Grid(nbPlayers);
+        Grid grid = new Grid(nbPlayers);
         System.out.println("grid.width = " + grid.width);
         System.out.println("grid.height = " + grid.height);
         grid.DisplayGrid();
