89ce947f0b2c350b877f3e8156a6d5e163ebddca
Ahrusalome <90481871+Ahrusalome@users.noreply.github.com>
2022-10-07 12:58:21 +0200

    player

--- puissance4/src/App.java
+++ puissance4/src/App.java
@@ -1,5 +1,29 @@
 public class App {
+    static int nbPlayers = 2;
+    static Grid game = new Grid();
+    static Player[] players = new Player[nbPlayers];
     public static void main(String[] args) throws Exception {
-
+        InitialiseGrid();
+        SetPlayers();
+        Play();
+    }
+    public static void InitialiseGrid() {
+        nbPlayers = Menu.chooseNbPlayers();
+        System.out.println("nbPlayers = " + nbPlayers);        
+        game.grid(nbPlayers);
+        game.BuildGrid();
+    }
+    public static void SetPlayers() {
+        for (int ii = 0; ii < nbPlayers; ii++){
+                players[ii] = new Player();
+                players[ii].setSymbol(ii);
+        }
+    }
+    public static void Play() {
+        for (int ii = 0; ii < players.length; ii++) {
+            game.DisplayGrid();
+            char column  = players[ii].getColumn(game.width);
+            game.PutPiece(players[ii].symbol, column-64);
+        }
     }
 }
--- puissance4/src/Player.java
+++ puissance4/src/Player.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class Player {
 
-    public char symbol;
+    public String symbol;
 
     public void setSymbol(int PlayerNB) {
         switch (PlayerNB) {
             case 0:
-            this.symbol = 'X';
+            this.symbol = "X";
             case 1:
-            this.symbol = 'O';
+            this.symbol = "O";
             case 2:
-            this.symbol = 'V';
+            this.symbol = "V";
         }
     }
 
@@ -19,13 +19,14 @@ public class Player {
         System.out.println("Which column do you want to choose ?");
         System.out.println(">");
         Scanner input = new Scanner(System.in);
-        input.close();
         if(input.nextLine().length() == 1 || input.nextLine().charAt(0) > (char)(width+64)) {
+            input.close();
             return input.nextLine().charAt(0);
         } else {
             System.out.println("Please enter a valid column name");
             getColumn(width);
         }
-        return 'a';
+        input.close();
+        return 'E';
     }
 }
