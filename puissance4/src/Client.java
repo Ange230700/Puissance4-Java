18eb88f9261b92e46bc50c02d0b709e795b1d155
Ahrusalome <90481871+Ahrusalome@users.noreply.github.com>
2022-10-22 17:42:32 +0200

    v3 ;)

--- puissance4/src/Client.java
+++ puissance4/src/Client.java
@@ -4,16 +4,14 @@ import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 
 public class Client {
-    static int ID = 10;
     public static void main(String[] args) {
-        ByteBuffer bytes = ByteBuffer.allocate(20);
+        ByteBuffer bytes = ByteBuffer.allocate(30);
         try {
             SocketChannel socket = SocketChannel.open();
             socket.connect(new InetSocketAddress("localhost", 4004));
             while (true) {
                 try {
                     System.out.println("Waiting for player(s) to connect...");
-                    ID = socket.read(bytes);
                     int nbPlayers = socket.read(bytes);
                     Menu.nbPlayers = nbPlayers;
                     App.game.grid(nbPlayers);
@@ -25,37 +23,15 @@ public class Client {
                             break;
                         }
                         bytes.clear();
-                        int turn = socket.read(bytes);
-                        bytes.clear();
-                        Menu.turnCounter = turn-1;
-                        System.out.println(turn + " turn");
-                        if ( turn == ID) {
-                            try {
-                                System.out.println("Your turn : ");
-                                Turn(socket,null, 0);
-                                turn++;
-                                WinCond.CheckWin(App.game);
-                                if (Menu.gameOver){
-                                    break;
-                                }
-                            } catch (Exception e) {
-                                System.err.println(e.toString());
-                            }
-                        } if ( turn == nbPlayers) {
-                            System.out.println("Server is playing...");
-                            int column = socket.read(bytes);
-                            bytes.clear();
-                            System.out.println(column);
+                        int message = socket.read(bytes);
+                        if (message == 14) {
+                            int column = Menu.getColumn(App.game);
+                            Turn(socket, null, column);
                             Menu.play(App.game, column);
-                            turn = 1;
-                        } 
-                        // if (turn != ID && turn != nbPlayers){
-                        //     System.out.println("oui");
-                        //     int column = socket.read(bytes);
-                        //     System.out.println(column);
-                        //     Menu.play(App.game, column);
-                        //     turn++;
-                        // }
+                        } else {
+                            Menu.play(App.game, message);
+                        }
+                        bytes.clear();
                     }
                 } catch (IOException e) {
                     System.err.println(e.toString());
@@ -68,20 +44,13 @@ public class Client {
             System.err.println(e.toString());
         }
     }
-    public static void Turn(SocketChannel sChannel, SocketChannel sChannel2, int columnClient){
+    public static void Turn(SocketChannel sChannel, SocketChannel sChannel2, int column){
         try {
-            if (columnClient == 0) {
-                int column = Menu.getColumn(App.game);
-                ByteBuffer bytes = ByteBuffer.wrap(new byte[(byte)column]);
-                sChannel.write(bytes);
-                if (sChannel2 != null) {
-                    ByteBuffer bytes2 = ByteBuffer.wrap(new byte[(byte)column]);
-                    sChannel2.write(bytes2);
-                }
-                Menu.play(App.game, column);
-            } else {
-                ByteBuffer bytes = ByteBuffer.wrap(new byte[(byte)columnClient]);
-                sChannel.write(bytes);
+            ByteBuffer bytes = ByteBuffer.wrap(new byte[(byte)column]);
+            sChannel.write(bytes);
+            if (sChannel2 != null) {
+                ByteBuffer bytes2 = ByteBuffer.wrap(new byte[(byte)column]);
+                sChannel2.write(bytes2);
             }
         } catch (IOException e) {
             System.err.println(e.toString());
--- puissance4/src/Server.java
+++ puissance4/src/Server.java
@@ -15,7 +15,7 @@ public class Server {
         server.launch();
     }
     public void launch() {
-        ByteBuffer bytes = ByteBuffer.allocate(20);
+        ByteBuffer bytes = ByteBuffer.allocate(30);
         try {
             ServerSocketChannel ssChan = ServerSocketChannel.open();
             ssChan.bind(new InetSocketAddress(4004));
@@ -26,9 +26,8 @@ public class Server {
                 clients.add(client);
                 ID++;
                 if (clients.size() == App.nbPlayers-1) {
-                    for (ClientHandler playingClient : clients) {
-                        ID(playingClient.socket, playingClient.ID);
-                        PlayersNb(playingClient.socket);
+                    for (ClientHandler _client : clients) {
+                        PlayersNb(_client.socket);
                     }
                     System.out.println("Game Starting");
                     App.game.DisplayGrid();
@@ -39,34 +38,31 @@ public class Server {
                             break;
                         }
                         for (ClientHandler playingClient : clients) {
-                            if (turn == playingClient.ID) {
-                                YourTurn(playingClient.socket);
-                                System.out.println("Another player is playing...");
-                                int clientColumn = playingClient.socket.read(bytes);
-                                bytes.clear();
-                                Menu.play(App.game, clientColumn);
-                                WinCond.CheckWin(App.game);
-                                turn++;
-                                System.out.println(turn +" turn après client");
-                                if (Menu.gameOver){
-                                    break;
+                            YourTurn(playingClient.socket);
+                            int clientColumn = playingClient.socket.read(bytes);
+                            if (App.nbPlayers == 3){
+                                if (playingClient == clients.get(0)) {
+                                    Client.Turn(clients.get(1).socket, null, clientColumn);
+                                } else {
+                                    Client.Turn(clients.get(0).socket, null, clientColumn);
                                 }
-                            } 
-                            // else {
-                            //     YourTurn(playingClient.socket);
-                            //     Client.Turn(playingClient.socket, null, 1);
-                            // }
-                                // break;
-                            // }
-                        }
-                        if (turn == clients.size()+1 && !Menu.gameOver){
-                            if (App.nbPlayers == 3) {
-                                Client.Turn(clients.get(0).socket, clients.get(1).socket, 0);
-                            } else {
-                                Client.Turn(clients.get(0).socket, null, 0);
                             }
-                            turn=1;
+                            Menu.play(App.game, clientColumn);
+                            WinCond.CheckWin(App.game);
+                            if (Menu.gameOver){
+                                break;
+                            }
                         }
+                        if (Menu.gameOver){
+                            break;
+                        }
+                        int column = Menu.getColumn(App.game);
+                        if (App.nbPlayers == 3) {
+                            Client.Turn(clients.get(0).socket, clients.get(1).socket, column);
+                        } else {
+                            Client.Turn(clients.get(0).socket, null, column);
+                        }
+                        Menu.play(App.game, column);
                     }
                 }
                 if (Menu.gameOver) {
@@ -79,15 +75,7 @@ public class Server {
     }
     public static void YourTurn(SocketChannel sChannel) {
         try {
-            ByteBuffer bytes = ByteBuffer.wrap(new byte[(byte)turn]);
-            sChannel.write(bytes);
-        } catch (IOException e){
-            System.err.println(e.toString());
-        }
-    }
-    public static void ID(SocketChannel sChannel, int clientID) {
-        try {
-            ByteBuffer bytes = ByteBuffer.wrap(new byte[(byte)clientID]);
+            ByteBuffer bytes = ByteBuffer.wrap(new byte[(byte)14]);
             sChannel.write(bytes);
         } catch (IOException e){
             System.err.println(e.toString());
